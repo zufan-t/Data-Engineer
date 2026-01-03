34ef13b4b3cb711cdaf7d3ea3b0d33b1dbb8d01f
zufan-t <zufantaufiqurrohman@gmail.com>
2026-01-03 18:59:44 +0700

    ulang

--- weather/dags/weather_sales_etl.py
+++ weather/dags/weather_sales_etl.py
@@ -7,11 +7,10 @@ import pandas as pd
 import json
 from google.oauth2 import service_account
 
-# --- KONFIGURASI ---
-PROJECT_ID = 'proyek1-476022' # Ganti Project ID kamu
-DATASET_RAW = 'raw_data_layer' # Dataset untuk data mentah
+PROJECT_ID = 'proyek1-476022'
+DATASET_RAW = 'raw_data_layer'
 API_KEY = '4f4cc497beb8ac56b8c43f4cd04c12a9'
-LAT = '-7.052' # UNNES
+LAT = '-7.052' 
 LON = '110.418'
 KEY_PATH = '/opt/airflow/keys/gcp_credentials.json'
 
@@ -23,13 +22,11 @@ default_args = {
 }
 
 def extract_weather_data(**kwargs):
-    # URL API untuk 5 day forecast
     url = f"https://api.openweathermap.org/data/2.5/forecast?lat={LAT}&lon={LON}&appid={API_KEY}&units=metric"
     
     response = requests.get(url)
     data = response.json()
     
-    # Ambil list forecast
     forecast_list = []
     for item in data['list']:
         forecast_list.append({
@@ -37,38 +34,34 @@ def extract_weather_data(**kwargs):
             'temp': item['main']['temp'],
             'feels_like': item['main']['feels_like'],
             'humidity': item['main']['humidity'],
-            'weather_main': item['weather'][0]['main'], # Rain, Clear, Clouds
+            'weather_main': item['weather'][0]['main'],
             'weather_desc': item['weather'][0]['description'],
             'ingestion_time': datetime.now()
         })
     
     df = pd.DataFrame(forecast_list)
     
-    # Load ke BigQuery Raw
     credentials = service_account.Credentials.from_service_account_file(KEY_PATH)
     df.to_gbq(
         destination_table=f'{DATASET_RAW}.unnes_weather_forecast',
         project_id=PROJECT_ID,
         credentials=credentials,
-        if_exists='replace' # Kita replace tiap hari biar fresh
+        if_exists='replace'
     )
     print("Data Weather Loaded to Raw Layer!")
 
 with DAG(
     'unnes_smart_sales_predictor',
     default_args=default_args,
-    schedule_interval='@daily', # Update tiap pagi buat persiapan jualan
+    schedule_interval='@daily',
     catchup=False
 ) as dag:
 
-    # 1. Ambil Data Cuaca
     t1_extract = PythonOperator(
         task_id='fetch_openweather_api',
         python_callable=extract_weather_data
     )
 
-    # 2. Transformasi Pakai dbt (Menghitung Prediksi)
-    # Kita jalankan perintah "dbt run" via terminal docker
     t2_dbt_run = BashOperator(
         task_id='dbt_transform_logic',
         bash_command='cd /opt/airflow/unnes_weather_dbt && dbt run --profiles-dir .'
